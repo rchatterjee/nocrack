70e574eed8c086a70bb200db1e0b35af6b9eba1f
Rahul Chatterjee <rchatterjee4@wisc.edu>
2013-11-25 10:54:58 -0600

    random generate password - PASSWORD CRACKER. Not working

--- honey_enc.py
+++ honey_enc.py
@@ -162,50 +162,9 @@ def Decode ( c, grammar ):
 def writePasswords ( p ):
     # writes the encoded passwords.. 
     f = open("password_vault.hny", 'w')
-
-def testRandomDecoding(grammar):
- #   print "Testing....:"
-    x = [ random.random() for x in range(PASSWORD_LENGTH) ]
-    c = struct.pack('%sf' % len(x), *x)
-    print Decode(c, grammar)
     
-def testEncoding ( grammar, trie ):
-    logF = open('logfile.txt', 'w')    
-    logF.write(
-"""
-# Dictionary: "<%s>"
-# File List : testdiclist.txt
-# top 1000 and randomly chosen 1000 from the rest and last 1000.
-""" % sys.argv[1]
-        )
-    files = ['../PasswordDictionary/passwords/' + s.strip() 
-             for s in open('testdiclist.txt').readlines() if not s[0]=='#']
-    for fname in files:
-        logF.write( "\n--------- %s ---------- :\n" % fname)
-        state, failed_count, total_tested, c = 0, 0, 0, 0
-        with bz2.BZ2File(fname) as f:
-            for l in f:
-                l = l.strip().split();
-                if len(l) > 1 and l[0].isdigit:
-                    l = ' '.join(l[1:]);
-                else:
-                    l = ' '.join(l);
-                if not l: continue;
-                if c>1000:
-                    if state == 2 : break
-                    else: state += 1;
-                    c=0;
-                if state is 0 or state is 2 or random.randint(0,1000)>90:
-                    c += 1;
-                    total_tested += 1
-                    if not Encode(l, trie, grammar): 
-                        failed_count += 1; 
-                        logF.write( l + "\n" )
-        logF.write( ">>>>>Total: %d\n>>>>>Failed: %d\n" % ( total_tested, failed_count) )
-    logF.close();
         
 import resource
-
 def main():
     grammar_flname, trie_flname = "data/grammar.hny.bz2", "data/trie.hny"
     if len (sys.argv) > 1 : 
@@ -219,7 +178,7 @@ def main():
     else:
         grammar, trie = loadDicAndTrie( grammar_flname, trie_flname );   
     print "Resource:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss;
-    testEncoding(grammar, trie); return;
+    # testEncoding(grammar, trie); return;
     p='rahulc12' # sys.stdin.readline().strip()
     c = Encode(p, trie, grammar);
     print "Encoding:", c
@@ -227,5 +186,6 @@ def main():
     m = Decode(c_struct, grammar);
     print "After Decoding:", m
     # for i in range(10): testRandomDecoding( grammar );
+
 if __name__ == "__main__":
     main();
