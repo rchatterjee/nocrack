dd53974f889b66d86eb73aaa03d992d5ae24b492
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-03-24 13:54:21 -0500

    Code for HoneyVault. Working and generating

--- honey_enc.py
+++ honey_enc.py
@@ -7,20 +7,113 @@ it needs a PCFG in the following format.
 
 import sys, os, math, struct, bz2, resource
 from io import BytesIO
-from mingle import *
 import random, marisa_trie
 from collections import deque
-
-
-PASSWORD_LENGTH = 50
-DEBUG = 1 # 1 S --> we are not getting combined rule like L3,D4 
-NONTERMINAL = 1
+from scanner_helper import *
+from scanner import *
 
 # IDEA:  every genrule could be kept in a Trie.
 # DRAWBACK: it is unlikely that those words will share
 # much prefix of each other...:p
 
 
+class DTE:
+    """
+    encodes a rule
+    """
+    def __init__(self):
+        self.g_struc = GrammarStructure()
+        self.term_files = {}
+        for k, f in self.g_struc.getTermFiles().items():
+            sys.path.append(GRAMMAR_DIR)
+            X = __import__('%s' % f)
+            self.term_files[k] = {
+                'trie' : marisa_trie.Trie().load(GRAMMAR_DIR+f+'.tri'),
+                'arr' : eval("X.%s"%k)
+                }
+        self.G = Grammar()
+        self.G.load(GRAMMAR_DIR+'/grammar.cfg')
+
+
+    @staticmethod
+    def word2prob( w, T, A ):       
+        i = T.key_id(unicode(w))
+        if i<0:
+            print "Could not find {w} in the trie.".format(**locals())
+            exit(0)
+        else:
+            S = sum( A[:i] )
+            t = random.randint( S, S+A[i] )
+            totalC = A[-1]
+            return t + random.randint(0, (4294967295-t)/totalC) * totalC
+
+    @staticmethod    
+    def prob2word( p, T, A, freq_also = False ):
+        i = getIndex(p, A)
+        w = T.restore_key(i)
+        return w if not freq_also else w, A[i]
+
+    def encode(self, lhs, rhs):
+        if  lhs in self.term_files:
+            return DTE.word2prob(rhs, self.term_files[lhs]['trie'],
+                             self.term_files[lhs]['arr'] )
+        assert lhs in self.G
+        rule_set = self.G[lhs][0]
+        freq_list= [ x[0] for x in self.G[lhs][1] ] # [[freq, type(N/T)]]
+        i = rule_set.index(rhs)
+        assert i>=0
+        S = sum( freq_list[:i] )
+        t = random.randint( S, S+freq_list[i] )
+        totalC = self.G[lhs][2]
+        return convert2group(t, totalC)
+    
+    def decode(self, lhs, prob, freq_also=False):
+        if  lhs in self.term_files:
+            w, f = DTE.prob2word(prob, 
+                                 self.term_files[lhs]['trie'],
+                                 self.term_files[lhs]['arr'], 
+                                 freq_also )
+            if freq_also:
+                return w, f, TERMINAL
+            else: 
+                return w, TERMINAL
+        
+        assert lhs in self.G
+        rule_set = self.G[lhs][0]
+        # print "KEYERROR Test:", self.G[lhs]
+        freq_list= [x[0] for x in self.G[lhs][1]]
+        freq_list.append( self.G[lhs][2] )
+        i = getIndex(prob, freq_list)
+        if freq_also:
+            return rule_set[i], freq_list[i], self.G[lhs][1][i][1]
+        else:
+            return rule_set[i], self.G[lhs][1][i][1]
+
+    def get_freq(self, lhs, rhs):
+        if lhs in self.term_files:
+            i = self.term_files[lhs]['trie'].key_id(unicode(rhs))
+            if i<0: 
+                print "KeyError in get_freq1:", lhs, rhs
+                return -1
+            return [self.term_files[lhs]['arr'][i], TERMINAL]
+        try:
+            i = self.G[lhs][0].index(rhs)
+            return self.G[lhs][1][i]
+        except KeyError: 
+            print "KeyError in get_freq2:", lhs, rhs
+            return -1
+    
+    def update_dte_for_vault(self, G):
+        self.term_files_bak = self.term_files
+        self.G_bak = self.G.G
+        self.term_files = {}
+        self.G.G = G;
+
+
+
+
+
+        
 def getVal( arr, val ):
     """
     arr -> is a list of values of type same as 'val' and also has a frequency
@@ -46,19 +139,19 @@ def getVal( arr, val ):
     if t>-1:
         # to deal with floating this is used, basically converted the numebrs in (mod totalC)
         # ASSUMPTION: max value of sum of frequency is 4,294,967,295, i.e. ~ 4.29 billion!!
-        return t + random.randint(0, (4294967295-t)/totalC) * totalC
+        return convert2group(t, totalC)
     return t
 
 
-def getIndex( arr, s, e, x ):
-    """
-    Binary searches for 'x' in 'arr' between indices 's' and 'e'.
-    :return `integer` index of x in arr s.t. arr[index-1] <= x < arr[index] 
-    """
-    if arr[s] > x: return s
-    if arr[e] < x: return e;
-    if arr[(s+e)/2] > x: return getIndex( arr, s, (s+e)/2, x )
-    else: return getIndex( arr, (s+e)/2+1, e, x);
+# def getIndex( arr, s, e, x ):
+#     """
+#     Binary searches for 'x' in 'arr' between indices 's' and 'e'.
+#     :return `integer` index of x in arr s.t. arr[index-1] <= x < arr[index] 
+#     """
+#     if arr[s] > x: return s
+#     if arr[e] < x: return e;
+#     if arr[(s+e)/2] > x: return getIndex( arr, s, (s+e)/2, x )
+#     else: return getIndex( arr, (s+e)/2+1, e, x);
 
 
 def getGenerationAtRule( lhs, prob, grammar):
@@ -82,6 +175,7 @@ def getGenerationAtRule( lhs, prob, grammar):
 
 def Encode_spcl( m, grammar ):
     print "Special Encoding::::", m
+    return None # TODO
     W = m # break_into_words(m, trie)
     P = ['%s%d' % (whatchar(w), len(w)) for w in W ]
     E = [];
@@ -92,26 +186,29 @@ def Encode_spcl( m, grammar ):
     E.append( getVal( grammar[P[-1]], W[-1]));
     if PASSWORD_LENGTH>0:
         extra = PASSWORD_LENGTH - len(E);
-        E.extend( [ random.randint(0, 4294967295) for x in range(extra) ] )
+        E.extend( [ convert2group(0,1) for x in range(extra) ] )
     return E;
 
 
-def Encode( m, tokenizer, grammar ):
-    P, W, U = tokenizer.tokenize(m, True)
-    E = []    
+def Encode( m, scanner, dte ):
+    P, W, U = scanner.tokenize(m, True)
+    E = []
     print P, W, U;
     # Grammar is of the form: S -> L3D2Y1 | L3Y2D5 | L5D2
-    t = getVal( grammar['S'], ','.join([ str(x) for x in P]) )
+    t = dte.encode('G', ','.join([ str(x) for x in P]) )
     print 't:', t; 
     if t==-1: # use the default .* parsing rule..:P 
+        print "Encode Spcl"
+        exit (0) 
         return Encode_spcl( m, grammar );
     else: E.append( t );
 
     for i,p in enumerate(P):
-        t=getVal(grammar[p], W[i])
+        t=dte.encode(p, W[i])
         if t==-1: 
             return Encode_spcl(m, grammar)
         E.append( t )
+        continue;
         if W[i].isalpha():
             for w,u in zip(W[i], U[i]):
                 try:
@@ -123,16 +220,17 @@ def Encode( m, tokenizer, grammar ):
                 except KeyError:
                     print 'here3', w, u
                     return Encode_spcl(m, grammar)
-    # print "Actual:", E;
+    print "Actual:", E;
     if PASSWORD_LENGTH>0:
         extra = PASSWORD_LENGTH - len(E);
-        E.extend( [ random.randint(0, 4294967295) for x in range(extra) ] )
-    return E
+        E.extend( [ convert2group(0,1) for x in range(extra) ] )
+    c_struct = struct.pack('%sI' % len(E), *E )
+    return c_struct
 
 
 # c is of the form set of numbers... 
 # probabilities, CDF
-def Decode ( c, grammar ):
+def Decode ( c, dte ):
     # c is io.BytesIO
     t = len( c );
     P = struct.unpack('%sI'%(t/4), c)
@@ -141,53 +239,48 @@ def Decode ( c, grammar ):
         
     plaintext = '';
     #queue = deque(['S']);
-    stack = ['S']
+    stack = ['G']
     for p in P:
-        #print stack, p
         try:
-            g = getGenerationAtRule( stack.pop(), p, grammar )
-        except: 
+            g = dte.decode( stack.pop(), p )
+        except IndexError: 
             #print plaintext, g
             #print "empty queue"
             break;
-        if g[1] == NONTERMINAL: 
+        if g[2] == NONTERMINAL: 
             arr = g[0].split(',')
             arr.reverse()
             stack.extend(arr)
-        elif g[1] == NONTERMINAL+1:
+        elif g[2] == NONTERMINAL+1:
             arr = list(g[0])
             arr.reverse()
             stack.extend(arr)
         else: # zero, terminal add 
             plaintext += g[0]
+        print g, stack, plaintext
     return plaintext
 
     
         
 def main():
-    base_dictionary, tweak_fl, passwd_dictionary, out_grmmar_fl, out_trie_fl = readConfigFile(sys.argv[1])
-    
-    _trie    = marisa_trie.Trie().load( out_trie_fl )
-    _tweaker = Tweaker(tweak_fl)
-    T = Tokenizer(tweaker=_tweaker, trie=_trie)
     #print T.tokenize('P@$$w0rd', True)
     #exit(0)
-    G = Grammar(tokenizer=T)
-    G.load(out_grmmar_fl)
+    dte = DTE()
+    scanner = Scanner()
     print "Resource:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss;
     #p='(NH4)2Cr2O7' # sys.stdin.readline().strip()
     p=u'iloveyou69'
-    c = Encode(p, T, G.G);
-    print "Encoding:", c
-    c_struct = struct.pack('%sI' % len(c), *c )
-    m = Decode(c_struct, G.G);
+    c = Encode(p, scanner, dte);
+    #print "Encoding:", c
+    m = Decode(c, dte);
     print "After Decoding:", m
+    #return
     #     if PASSWORD_LENGTH>0:
-    for s in range(10000):
+    for s in range(0000):
         E = []
-        E.extend( [ random.randint(0, 4294967295) for x in range(PASSWORD_LENGTH) ] )
+        E.extend( [ convert2group(0,1) for x in range(PASSWORD_LENGTH) ] )
         c_struct = struct.pack('%sI' % len(E), *E )
-        m = Decode(c_struct, G.G);
+        m = Decode(c_struct, dte);
         print s, ":", m
 
 
