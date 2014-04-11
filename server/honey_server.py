87f8758fb5c9a0450f507ff3ee16b233a341d225
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-04-11 15:23:08 -0500

    started server coding for honey enc

---
+++ server/honey_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+import sqlite3
+from flask import g
+DATABASE = '
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+def get_
+if __name__ == "__main__":
+    app.run()
