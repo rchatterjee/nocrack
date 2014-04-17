540e9a9dc0e9b0ea58d4b5a9cd61dc1d5836da29
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-04-17 17:16:01 -0500

    honey server working, honey_client to terminal based honey enc system

--- server/honey_server.py
+++ server/honey_server.py
@@ -1,24 +1,29 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, request
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.Protocol.KDF import PBKDF1
-from Crypto.Random import random, Random
+from Crypto.Random import random
 from Crypto.Util import Counter
 from Crypto import Random
-from binascii import hexlify
-import re, datetime
-from flask import Flask, request
+import binascii
+import re, datetime, sys
+sys.path.append('../')
+from honeyvault_config import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///honeyserver.db'
 db = SQLAlchemy(app)
 
 PRIVATE_HONEY_ENC_KEY = b"trha0Hmu@!$"
-PRIVATE_SALT = b"lukkaitlabon"
-EMAIL_REG = re.compile( r'''
-[a-z0-9!#$%&\'*+?^_~-]+(?:\.[a-z0-9!#$%&\'*+?^_`{|}~-]+)*
+PRIVATE_SALT = b"lukkaitlabon"[:8]
+EMAIL_REG = re.compile( r"""
+[a-z0-9!#$%&'*+?^_~-]+(?:\.[a-z0-9!#$%&'*+?^_`{|}~-]+)*
 @(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?
-''', re.MULTILINE )
+""", re.MULTILINE )
+
+
+b2a_base64 = lambda x: binascii.b2a_base64(x)[:-1]
 
 def hash_mp(mp):
     h = SHA256.new()
@@ -27,112 +32,160 @@ def hash_mp(mp):
 
 def do_crypto_setup():
     key = PBKDF1(PRIVATE_HONEY_ENC_KEY, PRIVATE_SALT, 16, 100, SHA256)
-    ctr = Counter.new(128, initial_value=long(254))
+    ctr = Counter.new(8*SECURITY_PARAM, initial_value=long(254))
     aes = AES.new(key, AES.MODE_CTR, counter=ctr)
     return aes
 
 global_saltgen_aes = do_crypto_setup()
 global_randfl = Random.new()
 
+def isEmail(username):
+    return (EMAIL_REG.match(username) is None)
+
+def isBlocked(ip):
+    return True
+
 def get_public_salt(username):
     return global_saltgen_aes.encrypt(hash_mp(username))
 
 class IPs(db.Model):
     ip = db.Column(db.String(25), primary_key=True)
-    lastaccessed = db.Column(db.Datetime, null=True, default=datetime.now())
+    lastaccessed = db.Column(db.DateTime, default=datetime.datetime.now())
     isblocked = db.Column(db.Boolean, default=False) 
 
-class User(db.Model):
+class tempUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True) # email-id is username
-    salt_pub = db.Column(db.String(128), null=False)
-    salt_sec = db.Column(db.String(128), null=False)
-    token    = db.Column(db.String(128), null=False)
-    vault_c  = db.Column(db.LargeBinary(HONEY_VAULT_ENCODING_SIZE))
-            
+    email_token = db.Column(db.String(SECURITY_PARAM_IN_BASE64), nullable=False)
+
     def __init__(self, username):
         self.username = username
-        self.salt_pub = get_public_salt(username)
-        self.salt_sec = global_randfl.read(16)
-        self.vault_c  = global_randfl.read(HONEY_VAULT_ENCODING_SIZE)
-        self.token    = global_randfl.read(128)
-        
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+        self.email_token = b2a_base64(global_randfl.read(SECURITY_PARAM))
+        print self.email_token
+    
     def save(self):
         db.session.add(self)
         db.session.commit()
 
-def isEmail(username):
-    return EMAIL_REG.match(username)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True) # email-id is username
+    salt_pub = db.Column(db.String(SECURITY_PARAM_IN_BASE64), nullable=False)
+    salt_sec = db.Column(db.String(SECURITY_PARAM_IN_BASE64), nullable=False)
+    bearer_token = db.Column(db.String(SECURITY_PARAM_IN_BASE64), nullable=False)
+    vault_c  = db.Column(db.String(int(HONEY_VAULT_ENCODING_SIZE*1.5)))
+            
+    def __init__(self, username):
+        self.username = username
+        self.salt_pub = b2a_base64(get_public_salt(username))
+        self.salt_sec = b2a_base64(global_randfl.read(SECURITY_PARAM))
+        self.bearer_token = b2a_base64(global_randfl.read(SECURITY_PARAM))
+        self.vault_c  = b2a_base64(global_randfl.read(int(HONEY_VAULT_ENCODING_SIZE*1.4)))
+        
+    def __repr__(self):
+        return '<User %r - <%r>>' % (self.username, self.bearer_token)
 
-def isBlocked(ip):
-    return True
+    def refresh_token(self):
+        self.bearer_token = b2a_base64(global_randfl.read(SECURITY_PARAM))
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-def GetToken(username):
-    if not isEmail(username):
-        pass
-    u = User(username=username)
+def Register(username):
+    if not username or not isEmail(username):
+        print "<%s> is not a valid Email!" % (username)
+        return "Sorry Not Valid Email"
+    u = tempUser.query.filter_by(username=username).first()
+    if not u:
+        u = tempUser(username=username)
     u.save()
-    return u.token
+    return u.email_token
 
-def Insert(username, token, mp_hash, vault=None):
-    u = User.query.filter_by(username=username)
-    if u and u.token == token:
-        if not vault:
-            vault = global_randfl.read(HONEY_VAULT_ENCODING_SIZE)
-            key = PBKDF1(mp_hash, u.salt_sec, 16, 100, SHA256)
-            ctr = Counter.new(128, initial_value=long(254))
-            aes = AES.new(key, AES.MODE_CTR, counter=ctr)
-        u.vault = aes.encrypt(vault)
+def ValidateEmail(username, email_token):
+    u = tempUser.query.filter_by(username=username).first()
+    if u and u.email_token == email_token:
+        U = User.query.filter_by(username=username).first()
+        if U:
+            U.refresh_token();
+        else:
+            U = User(username=username)
+
+        db.session.delete(u)
+        U.save()
+        return U.bearer_token 
+    else:
+        return "Wrong Token"
+
+def WriteVault(username, bearer_token, vault):
+    u = User.query.filter_by(username=username).first()
+    if u and u.bearer_token == bearer_token:
+        u.vault_c = vault_c
         u.save()
+        return "Done!"
+    return "Write is not allowed!"
 
-def GetPubSalt(username):
-    return get_public_salt(username)
+# mp_hash = hash(mp, slat_pub)
+def ReadVault(username, bearer_token):
+    u = User.query.filter_by(username=username).first()
+    print username, bearer_token
+    if u and u.bearer_token == bearer_token:
+        return u.vault_c
+    print u
+    return 'Read is not allowed!'
 
+def RefreshToken(username, bearer_token):
+    u = User.query.filter_by(username=username).first()
+    if u and u.bearer_token == bearer_token:
+        u.refresh_token()
+        u.save()
+        return u.bearer_token
+    return 'Not Permitted!'
 
 def GetWebsiteMapping():
     website_map = {
-        'account.google.com' : 1
+        'account.google.com' : 1,
         'yahoo.com' : 2,
         'facebook.com': 3
         }
     return json.encode( website_map );
-
-# mp_hash = hash(mp, slat_pub)
-def Retrieve(username, mp_hash):
-    u = User.query.filter_by(username=username)
-    if u:
-        key = PBKDF1(mp_hash, u.salt_sec, 16, 100, SHA256)
-        ctr = Counter.new(128, initial_value=long(254))
-        aes = AES.new(key, AES.MODE_CTR, counter=ctr)
-        return aes.decrypt(u.vault_c)
 
 
 @app.route("/")
 def hello():
     return "<h1>Hello Honey!</h1>"
 
-@app.route("/gettoken", methods=['GET'])
-def get_token(username):
-    username = request.args.get('username', '')
-    print GetToken(username)
-
-@app.route('/insert', methods=['POST'])
-def insert():
+@app.route("/register", methods=['POST'])
+def register():
     username = request.form.get('username', '')
-    token    = binascii.a2b_base64(request.form.get('token', ''))
-    mp_hash  = binascii.a2b_base64(request.form.get('mp_hash', ''))
-    vault_c  = binascii.a2b_base64(request.form.get('vault_c', '')) 
-    Insert(username, token, mp_hash, vault_c)
+    return Register(username)
 
-@app.route('/retrive', methods=['GET'])
-def retrive():
-    username = request.args.get('username', '')
-    mp_hash  = binascii.a2b_base64(request.form.get('mp_hash', ''))
-    print binascii.b2a_base64(Retrieve(username, mp_hash))
+@app.route('/verify', methods=['POST'])
+def validate_email():
+    username = request.form.get('username', '')
+    email_token = request.form.get('email_token', '')
+    return ValidateEmail(username, email_token)
 
+@app.route('/write', methods=['POST'])
+def write_vault():
+    username = request.form.get('username', '')
+    bearer_token = binascii.a2b_base64(request.form.get('token', ''))
+    vault_c  = request.form.get('vault_c', '')
+    return WriteVault(username, token, mp_hash, vault_c)
 
+@app.route('/read', methods=['POST'])
+def read_vault():
+    username = request.form.get('username', '')
+    bearer_token  = request.form.get('token', '')
+    return ReadVault(username, bearer_token)
+
+@app.route('/refresh', methods=['POST'])
+def refresh_token():
+    username = request.form.get('username', '')
+    bearer_token  = request.form.get('token', '')
+    return RefreshToken(username, bearer_token)
+
+    
 if __name__ == "__main__":
+    
     app.run(debug=True)
