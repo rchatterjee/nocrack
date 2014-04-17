85d1e4805560c6378f978f264d75e92d498e6aff
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-04-16 19:04:00 -0500

    Implemented the basic server functionalities, - with two level of encryption and implicit authentication.

---
+++ server/honeyapp/__init__.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('../')
+
+from flask import Flask
+from honeyvault_config import *
+
+app = Flask(__name__)
+
+app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'rchat@cs.wisc.edu'
+app.config["MAIL_PASSWORD"] = '1233456'
+
+from routes import mail
+mail.init_app(app)
+
+import intro_to_flask.routes
