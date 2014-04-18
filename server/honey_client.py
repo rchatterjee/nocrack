d71ca1dcca708340a6032973d2e3db493bac4b5b
Rahul Chatterjee <rchat@seclab6.cs.wisc.edu>
2014-04-18 17:09:39 -0500

    Temporary

--- server/honey_client.py
+++ server/honey_client.py
@@ -2,6 +2,8 @@ import os, sys
 import urllib, urllib2
 import binascii
 from Crypto.Hash import SHA256
+from urlparse import urlparse
+from publicsuffix import PublicSuffixList
 
 HONEY_SERVER_URL = "http://localhost:5000/"
 VAULT_FILE  = 'vault.db'
@@ -11,11 +13,18 @@ def create_request(sub_url, data):
     return urllib2.Request(HONEY_SERVER_URL+sub_url,
                            urllib.urlencode(data))
 
+def get_exact_domain( url ):
+    u = urlparse(url)
+    psl = PublicSuffixList()
+    return psl.get_public_suffix(u)
+
 def hash_mp(mp):
     h = SHA256.new()
     h.update(mp)
     return h.hexdigest()[:16]
 
+
+# ---------------- Client command line functions ------------------
 def register( *args ):
     h_string =  """
 cmd: register
@@ -27,6 +36,7 @@ e.g. $ %s -register badger@honey.com
     username = args[0]
     req = create_request('register', {'username': username})
     return urllib2.urlopen(req).read()
+
 
 def verify( *args ):    
     h_string =  """
@@ -107,11 +117,6 @@ e.g. $ %s -v badger@honey.com 'ThisIsTheToken007+3lL='
     req = create_request('getdomains', data)
     return urllib2.urlopen(req).read()
 
-    
-def default( *args ):
-    print '\n'.join("%s - %s" % (k,v) for k,v in command_func_map.items())
-    return "You are a moron!"
-
 
 def add_pass( *args ):
     h_string =  """
@@ -123,6 +128,7 @@ e.g. $ %s -addpass AwesomeS@la google.com 'FckingAwesome!'
         print h_string
         return ''
     
+
 def get_pass( *args ):
     h_string =  """
 cmd: get the saved password
@@ -137,6 +143,42 @@ e.g. $ %s -addpass AwesomeS@la google.com
     index = json.load(open(STATIC_DOMAIN_HASH_LIST)).get(domain, -1)
     
 
+def import_vault( *args ):
+    h_string =  """
+cmd: import existing vault, in given format
+$ %s -import <master-password> <vault_file>
+e.g. $ %s -import AwesomeS@la vault_file.csv
+vault file:
+# domain,username,password
+google.com,rahulc@gmail.com,abc234
+fb.com,rchatterjee,aadhf;123l
+""" % (sys.argv[0], sys.argv[0])
+    if len(args)<2:
+        print h_string
+        return ''
+    mp = args[0]
+    vault_fl = args[1]
+    # index = json.load(open(STATIC_DOMAIN_HASH_LIST)).get(domain, -1)
+
+
+def export_vault( *args ):
+    h_string =  """
+cmd: export the vault, 
+NOTE: this might generate some extra password which does not belong to you.
+Dont panic, those are randomly generated for security purpose. 
+Also for s2 part we need the cache, if it is not complete you will miss
+some in the export file, though password is there in the vault.
+$ %s -export <master-password>
+e.g. $ %s -export AwesomeS@la
+""" % (sys.argv[0], sys.argv[0])
+    if len(args)<2:
+        print h_string
+        return ''
+    mp = args[0]
+    domain = hash_mp(get_exact_domain(args[1]))
+    index = json.load(open(STATIC_DOMAIN_HASH_LIST)).get(domain, -1)
+
+    
 def gen_pass( *args ):
     h_string =  """
 cmd: generate random password
@@ -146,6 +188,12 @@ e.g. $ %s -addpass AwesomeS@la google.com
     if len(args)<2:
         print h_string
         return ''
+
+
+def default( *args ):
+    print '\n'.join("%s - %s" % (k,v) for k,v in command_func_map.items())
+    return "You are a moron!"
+    
 
 command_func_map = {
     '-register' : register,
@@ -157,11 +205,15 @@ command_func_map = {
     '-getdomainhash' : get_static_domains,
     '-addpass' : add_pass,
     '-getpass' : get_pass,
-    'genpass' : gen_pass,
+    '-genpass' : gen_pass,
+    '-import' : import_vault,
+    '-export' : export_vault,
 }
 
-if len(sys.argv)>1:
-    cmd = sys.argv[1]
-    print command_func_map.get(cmd, default)(*sys.argv[2:])
-else:
-    default( sys.argv[1:] )
+
+if __name__ == "__main__":
+    if len(sys.argv)>1:
+        cmd = sys.argv[1]
+        print command_func_map.get(cmd, default)(*sys.argv[2:])
+    else:
+        default( sys.argv[1:] )
